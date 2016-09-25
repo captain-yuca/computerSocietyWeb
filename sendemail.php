4ba9a634b74aa42a896f9885707c998387bfe084
Manuel Baez <Manuel Baez>
2016-09-24 20:56:45 -0400

    Main Revision for Company Night

--- sendemail.php
+++ sendemail.php
@@ -1,17 +1,20 @@
 <?php
 	header('Content-type: application/json');
+    header ("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Methods: PUT, GET, POST");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 	$status = array(
 		'type'=>'success',
 		'message'=>'Thank you for contact us. As early as possible  we will contact you '
 	);
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    $name = @trim(stripslashes($_POST['name']));
+    $email = @trim(stripslashes($_POST['email']));
+    $subject = @trim(stripslashes($_POST['subject']));
+    $message = @trim(stripslashes($_POST['message']));
 
     $email_from = $email;
-    $email_to = 'email@email.com';//replace with your email
+    $email_to = 'computersociety@uprm.edu';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
